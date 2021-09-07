78d0f1d7f024ad0a193539d1aef4952289c2b9ed
Orkhai Dickson <dicksonorkhai@gmail.com>
2021-09-07 13:14:32 +0100

    Completed README

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const toggle = document.querySelector("input");
 const header = document.querySelector("header");
+const body = document.querySelector("body");
 const heading = document.querySelector("h1");
+const followerCount = document.querySelectorAll("h3");
+const smCards = document.querySelectorAll(".sm-accounts div");
 
 toggle.addEventListener("input", () => {
   header.classList.toggle("dark-mode-header");
   heading.classList.toggle("dark-mode-text");
+  body.classList.toggle("dark-mode-body");
+  for (const card of smCards) {
+    card.classList.toggle("dark-mode-card");
+  }
+  for (const count of followerCount) {
+    count.classList.toggle("dark-mode-text");
+  }
 });
